ca4772818bbd37e93739e236a021b7a6b872a39f
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2023-07-02 02:09:34 +0530

    project to do list

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+var a = __dirname + "public"
+app.use(express.static(__dirname + "/public"));
+
+
+var items = ["Food", "Book Reading", "Eat"];
+
+app.get("/", (req, res) => {
+    var today = new Date();
+    var options = {
+        weekday:"long", 
+        day:"numeric", 
+        month:"long"
+    }
+    var date = today.toLocaleDateString("en-IN", options)
+
+    res.render("dashboard", {KindOfDate: date, itemsTotal: items});
+});
+
+app.post("/", (req, res)=>{
+    var item = req.body.item;
+    items.push(item);
+    res.redirect("/");
+});
+app.listen(3000, (error) => {
+    console.log('Server is running on port 3000 ' + error);
+});
